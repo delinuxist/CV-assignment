0f58975c9c832e40e3ecca4aa9e3c487a316c6de
delinuxist <delinuxist.gmail.com>
2022-04-18 01:26:06 +0000

    contains all exported components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+import Personal from "./components/Personal";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className=" w-full h-[64px] bg-slate-400 ">
+        <h1 className=" text-center py-4 text-3xl">CV PROJECT</h1>
+      </nav>
+      <div className="container mx-auto pb-5">
+        <Personal />
+        <Education />
+        <Experience />
+      </div>
+    </>
   );
 }
 
